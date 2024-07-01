3e6ab05a20e476e4c96b070caca643d99ab76876
Sabit Khan <mesabitkhan@gmail.com>
2024-07-01 10:20:26 +0500

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+const numberArray = [0, 1, 2, 3, 4, 5];
+
+const myHero = ["A", "B", "C"];
+
+const myArray = new Array(1, 2, 3, 4, 5, 6);
+
+// console.log(myArray[2]);
+
+// array method
+
+// myArray.push(7);
+// console.log(myArray);
+// myArray.pop();
+// console.log(myArray);
+// myArray.shift();
+// console.log(myArray.includes(0));
+// console.log(myArray.indexOf(4));
+// console.log(myArray);
+
+console.log("A ", myArray);
+const myNewArray1 = myArray.slice(0, 3);
+console.log("B ", myArray);
+console.log(myNewArray1);
+const myNewArray2 = myArray.splice(0, 3);
+console.log(myNewArray2);
+console.log("c ", myArray);
