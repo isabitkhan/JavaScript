7ccdddebae6bb4dcf8d05f9ff6df5a9d17c22eab
Sabit Khan <mesabitkhan@gmail.com>
2024-07-01 10:59:26 +0500

    more related to array complex situation

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -17,10 +17,28 @@ const myArray = new Array(1, 2, 3, 4, 5, 6);
 // console.log(myArray.indexOf(4));
 // console.log(myArray);
 
-console.log("A ", myArray);
-const myNewArray1 = myArray.slice(0, 3);
-console.log("B ", myArray);
-console.log(myNewArray1);
-const myNewArray2 = myArray.splice(0, 3);
-console.log(myNewArray2);
-console.log("c ", myArray);
+// console.log("A ", myArray);
+// const myNewArray1 = myArray.slice(0, 3);
+// console.log("B ", myArray);
+// console.log(myNewArray1);
+// const myNewArray2 = myArray.splice(0, 3);
+// console.log(myNewArray2);
+// console.log("c ", myArray);
+
+const marvel_heros = ["ironman", "captain", "spiderman"];
+const dc_heros = ["superman", "flash", "batman"];
+
+const newHerosArray = marvel_heros.concat(dc_heros);
+console.log(newHerosArray);
+
+const newArray = [...marvel_heros, ...dc_heros];
+console.log(newArray);
+
+console.log(Array.isArray("Sanit"));
+console.log(Array("Sanit"));
+console.log(Array.from("Sanit"));
+
+const complexSituation = [1, 2, 3, ["Sabit", "amir", [1, "Ziyad"]], ["0348"]];
+
+const solved = complexSituation.flat(Infinity);
+console.log(solved);
